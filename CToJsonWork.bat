6d95f06e0c47ac20cc631611ed50f7a18b681e5c
Greg.Saltis <greg.saltis@vertiv.com>
2023-05-09 11:08:34 -0400

    Separate Batch files

---
+++ CToJsonWork.bat
@@ -0,0 +1,5 @@
+set ARMINCLUDE=c:/Users/greg.saltis/Source/arm926/host-cross/arm-wrs-linux-gnueabi/sysroot/usr/include/
+set NCUINCLUDE=c:/Users/greg.saltis/Source/NCUCodeMerge/Track2_NCU_Code-master/src/include/
+set NCUBASE=C:/users/greg.saltis/Source/NCUCodeMerge/Track2_NCU_Code-master/src
+
+clang -c -isystem %ARMINCLUDE% -Xclang -ast-dump=json -I%NCUINCLUDE% -ferror-limit=9999 %NCUBASE%/%1/%2 > %2.json 2> %2.errors
